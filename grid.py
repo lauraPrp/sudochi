10b77d3c06ef3b507a68e93466a0a6d43cae9725
lauraPrp <laura.porpiglia@gmail.com>
2022-05-12 09:21:09 +0100

    works at 3rd click

--- grid.py
+++ grid.py
@@ -8,7 +8,7 @@ def create_grid():
     for j in range(9):
         row = []
         for i in range(9):
-            cell = Cell(j,i,random.randint(1, 9))
+            cell = Cell(j,i,random.randint(1, 9),"255,255,255", False)
             row.append(cell)
         grid.append(row)
     return grid
--- main.py
+++ main.py
@@ -14,7 +14,6 @@ x1 = 0
 y1 = 0
 
 dif = 500 / 9
-val = 0
 flagPress1 = 0
 flagPress2 = 0
 
@@ -23,6 +22,9 @@ grid = grid.create_grid()
 # Load test fonts for future use
 font1 = pygame.font.SysFont("verdana", 30)
 font2 = pygame.font.SysFont("verdana", 10)
+WHITE = 255, 255, 255
+RED = 255, 0, 0
+BLACK = 0, 0, 0
 
 
 def get_cord_first_click(pos):
@@ -56,12 +58,12 @@ def get_cord_second_click(pos):
 # Highlight the cell selected
 def highlight_cell():
     # draw red lines vert/hor to highlight the cell
-    if flagPress1 != 0 and flagPress2 != 0:
-        for i in range(2):
-            pygame.draw.line(screen, (255, 0, 0), (x * dif - 3, (y + i) * dif), (x * dif + dif + 3, (y + i) * dif), 7)
-            pygame.draw.line(screen, (255, 0, 0), ((x + i) * dif, y * dif), ((x + i) * dif, y * dif + dif), 7)
-            pygame.draw.line(screen, (255, 0, 0), (x1 * dif - 3, (y1 + i) * dif), (x1 * dif + dif + 3, (y1 + i) * dif), 7)
-            pygame.draw.line(screen, (255, 0, 0), ((x1 + i) * dif, y1 * dif), ((x1 + i) * dif, y1 * dif + dif), 7)
+
+    for i in range(2):
+        pygame.draw.line(screen, (RED), (x * dif - 3, (y + i) * dif), (x * dif + dif + 3, (y + i) * dif), 7)
+        pygame.draw.line(screen, (RED), ((x + i) * dif, y * dif), ((x + i) * dif, y * dif + dif), 7)
+        pygame.draw.line(screen, (RED), (x1 * dif - 3, (y1 + i) * dif), (x1 * dif + dif + 3, (y1 + i) * dif), 7)
+        pygame.draw.line(screen, (RED), ((x1 + i) * dif, y1 * dif), ((x1 + i) * dif, y1 * dif + dif), 7)
 
 
 def draw():
@@ -69,47 +71,23 @@ def draw():
     for i in range(9):
         for j in range(9):
             cell = grid[i][j]
-            if cell.value != 0:
-                # Fill blue color in already numbered grid
-                # pygame.draw.rect(screen, (0, 153, 153), (i * dif, j * dif, dif + 1, dif + 1))
 
-                # Fill grid with default numbers specified
-                text1 = font1.render(str(cell.value), 1, (0, 0, 0))
-                screen.blit(text1, (i * dif + 10, j * dif + 10))
-            elif cell.value == 0:
-                # Fill blue color in already numbered grid
-                pygame.draw.rect(screen, (0, 153, 153), (i * dif, j * dif, dif + 1, dif + 1))
-                # Draw lines to form grid
+            pygame.draw.rect(screen, WHITE, (i * dif, j * dif, dif + 1, dif + 1))
+            text1 = font1.render(str(cell.value), 1, (BLACK))
+            screen.blit(text1, (i * dif + 10, j * dif + 10))
+            # Draw lines to form grid
     for i in range(10):
         thick = 1
         # horiz
-        pygame.draw.line(screen, (0, 0, 0), (0, i * dif), (500, i * dif), thick)
+        pygame.draw.line(screen, (BLACK), (0, i * dif), (500, i * dif), thick)
         # vertical
-        pygame.draw.line(screen, (0, 0, 0), (i * dif, 0), (i * dif, 500), thick)
-
-
-# Fill value entered in cell
-def draw_val(val):
-    text1 = font1.render(str(val), 1, (0, 0, 0))
-    screen.blit(text1, (x * dif + 15, y * dif + 15))
+        pygame.draw.line(screen, (BLACK), (i * dif, 0), (i * dif, 500), thick)
 
 
 # Display instruction for the game
 def instruction():
-    text1 = font2.render("(SHOULD) MATCH ADIACENTS EQUALS NUMBERS AND THOSE WHOSE SUM IS 10", 1, (0, 0, 0))
+    text1 = font2.render("(SHOULD) MATCH ADJACENTS EQUALS NUMBERS AND THOSE WHOSE SUM IS 10", 1, (BLACK))
     screen.blit(text1, (20, 520))
-
-
-def check_pressed(flag1, flag2):
-    if flag1 == 0 and flag2 == 0:
-        return False
-    else:
-        return True
-
-
-def reset_pressed():
-    flagPress1 = 0
-    flagPress2 = 0
 
 
 run = True
@@ -119,7 +97,7 @@ while run:
     # pos1 = 0, 0
     # pos2 = 0, 0
     # White color background
-    screen.fill((255, 255, 255))
+    screen.fill(WHITE)
 
     # Loop through the events stored in event.get()
     for event in pygame.event.get():
@@ -134,12 +112,18 @@ while run:
             # setPressed
             if flagPress1 == 0:
                 flagPress1 = 1
-                get_cord_first_click(pos)
+
+                cell = get_cord_first_click(pos)
+                #cell.is_selected = True
+                grid[cell.x_coord][cell.y_coord] = cell
                 pos1 = pos
+
             elif flagPress1 == 1 and flagPress2 == 0:
                 flagPress2 = 1
                 pos2 = pos
-                get_cord_second_click(pos)
+                cell = get_cord_second_click(pos)
+                #cell.is_selected = True
+                grid[cell.x_coord][cell.y_coord] = cell
             elif flagPress1 == 1 and flagPress2 == 1:
                 # evaluate
 
@@ -147,17 +131,22 @@ while run:
                 pos2 = pos
                 cell1 = get_cord_first_click(pos1)
                 cell2 = get_cord_second_click(pos)
+                #cell1.is_selected = True
+                #cell2.is_selected = True
+                grid[cell1.x_coord][cell1.y_coord] = cell1
+                grid[cell2.x_coord][cell2.y_coord] = cell2
                 print("reset " + str(cell1.value) + str(cell2.value))
                 flagPress1 = 0
                 flagPress2 = 0
                 if cell1.value == cell2.value:
+                    # check if valid
                     print("SAME")
+                    cell1.value = 0;
+                    cell2.value = 0;
 
     draw()
     instruction()
-    if check_pressed:
-        highlight_cell()
-
+    highlight_cell()
     # Update window
     pygame.display.update()
 
