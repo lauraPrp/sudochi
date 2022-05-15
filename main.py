56111fc10aeac16b12e4d5da43105739fb85560f
lauraPrp <laura.porpiglia@gmail.com>
2022-05-15 15:39:09 +0100

    added sum to 10 and is_near function

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ y1 = 0
 dif = 500 / 9
 flagPress1 = 0
 flagPress2 = 0
-flag_started = False
+flag1_started = False
+flag2_started = False
 
 grid = grid.create_grid()
 
@@ -62,12 +63,13 @@ def get_cord_second_click(pos):
 # Highlight the cell selected
 def highlight_cells():
     # draw red lines vert/hor to highlight the cell
-    if(flag_started):
+    if (flag1_started):
         for i in range(2):
             pygame.draw.line(screen, RED, (x * dif - 3, (y + i) * dif), (x * dif + dif + 3, (y + i) * dif), 7)
             pygame.draw.line(screen, RED, ((x + i) * dif, y * dif), ((x + i) * dif, y * dif + dif), 7)
-            pygame.draw.line(screen, RED, (x1 * dif - 3, (y1 + i) * dif), (x1 * dif + dif + 3, (y1 + i) * dif), 7)
-            pygame.draw.line(screen, RED, ((x1 + i) * dif, y1 * dif), ((x1 + i) * dif, y1 * dif + dif), 7)
+            if (flag2_started):
+                pygame.draw.line(screen, RED, (x1 * dif - 3, (y1 + i) * dif), (x1 * dif + dif + 3, (y1 + i) * dif), 7)
+                pygame.draw.line(screen, RED, ((x1 + i) * dif, y1 * dif), ((x1 + i) * dif, y1 * dif + dif), 7)
 
 
 def draw():
@@ -75,11 +77,15 @@ def draw():
     for i in range(9):
         for j in range(9):
             cell = grid[i][j]
+            if cell.value == 0:
+                # Fill blue color in already numbered grid
+                pygame.draw.rect(screen, WHITE, (i * dif, j * dif, dif + 1, dif + 1))
 
-            pygame.draw.rect(screen, WHITE, (i * dif, j * dif, dif + 1, dif + 1))
-            text1 = font1.render(str(cell.value), 1, (BLACK))
-            screen.blit(text1, (i * dif + 10, j * dif + 10))
-            # Draw lines to form grid
+            else:
+                pygame.draw.rect(screen, WHITE, (i * dif, j * dif, dif + 1, dif + 1))
+                text1 = font1.render(str(cell.value), 1, (BLACK))
+                screen.blit(text1, (i * dif + 10, j * dif + 10))
+                # Draw lines to form grid
     for i in range(10):
         thick = 1
         # horiz
@@ -93,8 +99,26 @@ def instruction():
     text1 = font2.render("(SHOULD) MATCH ADJACENTS EQUALS NUMBERS AND THOSE WHOSE SUM IS 10", 1, BLACK)
     screen.blit(text1, (20, 520))
 
-def is_near():
-    print("not fully implemented yet")
+
+def is_near(initial_cell, landing_cell):
+    # bishop like move
+    dx = abs(initial_cell.x_coord - landing_cell.x_coord)
+    dy = abs(initial_cell.y_coord - landing_cell.y_coord)
+    if abs(initial_cell.x_coord - landing_cell.x_coord == 1) and (initial_cell.y_coord - landing_cell.y_coord == 0):
+        return True
+    elif abs(landing_cell.x_coord - initial_cell.x_coord == 1) and (initial_cell.y_coord - landing_cell.y_coord == 0):
+        return True
+    elif abs(initial_cell.y_coord - landing_cell.y_coord == 1) and (initial_cell.x_coord - landing_cell.x_coord == 0):
+        return True
+    elif abs(landing_cell.y_coord - initial_cell.y_coord == 1) and (initial_cell.x_coord - landing_cell.x_coord == 0):
+        return True
+    elif (dx == dy) and (dx > 0):
+
+        return True
+    else:
+        print("not legal move")
+        return False
+
 
 run = True
 
@@ -112,33 +136,37 @@ while run:
             run = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            flag_started = True
+
             pos = pygame.mouse.get_pos()
 
             # setPressed
             if flagPress1 == 0:
                 flagPress1 = 1
+                flag1_started = True
 
                 first_cell = get_cord_first_click(pos)
-                #first_cell.is_selected = True
+                # first_cell.is_selected = True
                 grid[first_cell.x_coord][first_cell.y_coord] = first_cell
 
             elif flagPress1 == 1 and flagPress2 == 0:
                 flagPress2 = 1
 
                 scd_cell = get_cord_second_click(pos)
-                #cell.is_selected = True
+                flag2_started = True
+                # cell.is_selected = True
                 grid[scd_cell.x_coord][scd_cell.y_coord] = scd_cell
 
-
                 print("DEBUG reset " + str(first_cell.value) + str(scd_cell.value))
+                # bug 3rd click
                 flagPress1 = 0
                 flagPress2 = 0
-                if first_cell.value == scd_cell.value:
+                if (first_cell.value == scd_cell.value) or (first_cell.value + scd_cell.value == 10):
                     # check if valid
                     print("DEBUG SAME")
-                    first_cell.value = 0;
-                    scd_cell.value = 0;
+                    if (is_near(first_cell, scd_cell)):
+                        first_cell.value = 0
+                        scd_cell.value = 0
+                    # disable cells
 
     draw()
     instruction()
